5debaa8f08680097b83da4d9319e0fa31ae6338a
Koretzkiy Bogdan <koretzki.bogdan@gmail.com>
2017-01-07 01:10:11 +0200

    set db connection for travis env

--- config/connections.js
+++ config/connections.js
@@ -20,7 +20,9 @@ let connections = {
       hostname: process.env.HOST || '127.0.0.1'
     },
     database: {
-      url: 'postgres://rqddyyqrtzswki:f04c8adb9203f58c99708c270d1cc28dec04ce632d84bb5af6a1b549867355f0@ec2-46-137-117-43.eu-west-1.compute.amazonaws.com:5432/dela6s3hrodjgt',
+      // test on travis
+      url: process.env.DB_URL,
+      // url 'postgres://rqddyyqrtzswki:f04c8adb9203f58c99708c270d1cc28dec04ce632d84bb5af6a1b549867355f0@ec2-46-137-117-43.eu-west-1.compute.amazonaws.com:5432/dela6s3hrodjgt',
       options: {
         dialect: 'postgres',
         logging: false,
